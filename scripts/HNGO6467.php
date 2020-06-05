d9b03a6a550a97a4701cd2ce5ee372362db96cbc
adekunlebasirat <38426049+adekunlebasirat@users.noreply.github.com>
2020-06-05 11:30:42 +0100

    updated HNG-06467.php
    i added my full name and my HNG code

--- scripts/HNGO6467.php
+++ scripts/HNGO6467.php
@@ -1,6 +1,6 @@
 <?php
 $hng_id ="HNG-06467" ;
-$name ="taiwo" ;
+$name ="Adekunle Taiwo Adenike";
 $language = "php";
-echo "Hello World, this is [$name] with HNGi7 ID [$hng_id] using [$language]  for stage 2 task";
+echo "Hello World, this is ".$name." with HNGi7 ID ".$hng_id." using ".$language."  for stage 2 task.";
 ?>
